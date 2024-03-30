5f8cf6fa7ce745f608b5a5034679571c01bb86e1
server <server@DESKTOP-2JJBBUB>
2024-03-30 11:08:45 +0300

    yenileme

--- Projeler/src/Proje/ilkProje.java
+++ Projeler/src/Proje/ilkProje.java
@@ -1,11 +1,23 @@
 package Proje;
 
+import java.io.File;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
 public class ilkProje {
+  
 
 	public static void main(String [] args)
 	{
-		System.out.println(Math.max(-4.7,8));
-		System.out.println(Math.max(-4.7,8));
-
-    }
+		 Scanner in = new Scanner(System.in);
+		int arr[][]= {{13,9,73},{4,2},{8}};
+		
+		for(int row = 0 ; row<arr.length; row++)
+		{
+			for(int col = 0 ; col < arr[row].length; col++)
+				System.out.print(arr[row][col]+ " ");	
+		}
+		System.out.println();	
+	}
 }
+	
